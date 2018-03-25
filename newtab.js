3f2ac5f03076f7deb94d9091e8be53f1b3c7fb9c
Mario Kunz <mario99.kunz@gmail.com>
2018-03-25 16:18:57 +0200

    place info content in another place

--- newtab.js
+++ newtab.js
@@ -1,30 +1,37 @@
-var API_KEY = 'xIzkrseZNgpS0qsCZZwomBsofc9I5e0FUl3GAjvH';
-var startDate = new Date();
+let API_KEY = 'xIzkrseZNgpS0qsCZZwomBsofc9I5e0FUl3GAjvH';
+let startDate = new Date();
 startDate.setDate(startDate.getDate() - 5);
-var dateString = startDate.toISOString();
-var apiUrl = 'https://api.nasa.gov/planetary/apod?api_key=' + API_KEY + '&start_date=' + dateString.slice(0, dateString.indexOf('T'));
+let dateString = startDate.toISOString();
+let apiUrl = 'https://api.nasa.gov/planetary/apod?api_key=' + API_KEY + '&start_date=' + dateString.slice(0, dateString.indexOf('T'));
 
-var container = document.getElementById('unix-time');
+let apodWrapper = document.getElementById('apod-wrapper');
+let infoTitle = document.getElementById('info-title');
+let infoText = document.getElementById('info-text');
+let info = document.getElementById('info');
+let content = document.getElementById('info-content');
+let infoCopyright = document.getElementById('info-copyright');
+let infoTitleContainerHeight = document.getElementById('info-title-container').scrollHeight;
+
+let time = document.getElementById('time');
+
 setInterval(function () {
-    container.innerHTML = new Date().toLocaleTimeString();
+    time.innerHTML = new Date().toLocaleTimeString();
 }, 1000);
-
-var xmlHttpRequest = new XMLHttpRequest();
+let xmlHttpRequest = new XMLHttpRequest();
 xmlHttpRequest.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-        var resp = JSON.parse(xmlHttpRequest.responseText);
-        var randomImage = resp[Math.round(Math.random() * 5)];
 
+    if (this.readyState === 4 && this.status === 200) {
+        let resp = JSON.parse(xmlHttpRequest.responseText);
+        let randomImage = resp[Math.round(Math.random() * 5)];
         if (randomImage['media_type'] === 'image') {
-            var imageUrl = randomImage['hdurl'] !== undefined ? randomImage['hdurl'] : randomImage['url'];
-            document.getElementById('apod-wrapper').setAttribute('style', 'background-image: url(' + imageUrl + ');');
+            let imageUrl = randomImage['hdurl'] !== undefined ? randomImage['hdurl'] : randomImage['url'];
+            apodWrapper.setAttribute('style', 'background-image: url(' + imageUrl + ');');
         } else if (randomImage['media_type'] === 'video') {
-            var video = document.createElement('iframe');
+            let video = document.createElement('iframe');
             video.setAttribute('src', randomImage['url'] + (randomImage['url'].includes('?') ? '&' : '?') + 'autoplay=1');
             video.setAttribute('width', window.innerWidth);
-            video.setAttribute('height', window.innerHeight);
-            document.getElementById('apod-wrapper').appendChild(video);
-
+            video.setAttribute('height', window.innerHeight - infoTitleContainerHeight);
+            apodWrapper.appendChild(video);
             try {
                 video.mute();
             } catch (e) {
@@ -33,11 +40,16 @@ xmlHttpRequest.onreadystatechange = function () {
             console.warn('Unknown media type: ' + randomImage['media_type']);
         }
 
-        document.getElementById('info-title').innerText = randomImage['title'];
-        document.getElementById('info-text').innerText = randomImage['explanation'];
-        document.getElementById('info-copyright').innerHTML = '&copy; ' + randomImage['copyright'];
+        infoTitle.innerText = randomImage['title'];
+        infoText.innerText = randomImage['explanation'];
+        console.log(randomImage['copyright']);
+        if (randomImage['copyright'] !== undefined) {
+            infoCopyright.innerHTML = '&copy; ' + randomImage['copyright'];
+        } else {
+            infoText.style.paddingBottom = '16px';
+        }
 
-        var title = document.createElement('h3');
+        let title = document.createElement('h3');
         title.appendChild(document.createTextNode(new Date(randomImage['date']).toLocaleDateString()));
         document.getElementById('title').appendChild(title);
     }
@@ -45,3 +57,10 @@ xmlHttpRequest.onreadystatechange = function () {
 xmlHttpRequest.open('GET', apiUrl);
 xmlHttpRequest.send();
 
+info.addEventListener('mouseenter', function () {
+    content.style.height = content.scrollHeight + 'px';
+});
+
+info.addEventListener('mouseleave', function () {
+    content.style.height = '0';
+});
